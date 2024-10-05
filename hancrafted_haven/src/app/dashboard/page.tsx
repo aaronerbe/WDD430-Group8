6c1985f46476ab08dd2ffb08ba7da1500667b9df
lagoke <72529997+lagoke@users.noreply.github.com>
2024-10-05 18:32:27 -0400

    handcraft nav

---
+++ hancrafted_haven/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+     return (
+     <main>
+    <span>Sellers</span> <br />
+    <span>Products</span><br />
+    <span>Review & Ratings</span><br />
+    <span>Sign out >></span><br />
+     </main>
+    
+     )
+    }
+    
