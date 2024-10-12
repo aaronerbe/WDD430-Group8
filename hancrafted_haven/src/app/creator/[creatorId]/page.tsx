b57236313681c12f7ba6bcc1a491247abb8980a0
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-10-12 12:14:49 -0400

    Putting in dummy data so vercel build will be happy.  this file page.tsx will be replace

--- hancrafted_haven/src/app/creator/[creatorId]/page.tsx
+++ hancrafted_haven/src/app/creator/[creatorId]/page.tsx
@@ -1,1 +1,20 @@
-//THIS IS A DUMMY PAGE TO BE REPLACED BY AMIOLAS CODE
+//THIS IS A TEMP FILE.  TO BE REPLACED
+
+'use client'
+import { useParams } from 'next/navigation';
+
+export default function CreatorPage() {
+    const { creatorId } = useParams();
+
+    return (
+        <main>
+            <h1>Creator Details</h1>
+            <p>Creator ID: {creatorId}</p>
+
+            <div>Image</div>
+            <div>About Me</div>
+            <div>Creators Work</div>
+            <div>Curated Work</div>
+        </main>
+    )
+}
