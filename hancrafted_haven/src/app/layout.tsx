6d3459704126cd5650a18216a931fe57121a8f43
bcushenberry <cushenberry@gmail.com>
2024-10-15 02:24:34 +0900

    Initial commit for the navbar

--- hancrafted_haven/src/app/layout.tsx
+++ hancrafted_haven/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Footer from './Footer';
-
+import Navbar from './Navbar';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
         <Footer />
       </body>
