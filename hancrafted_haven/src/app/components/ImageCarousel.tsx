dbd527f47e7e4b5f4d3ed5497eecd24b46a1e14a
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-10-15 17:23:29 -0400

    Add default image management.  If error finding image

--- hancrafted_haven/src/app/components/ImageCarousel.tsx
+++ hancrafted_haven/src/app/components/ImageCarousel.tsx
@@ -1,12 +1,7 @@
 'use client';
 import Image from 'next/image'; 
-//import { fetchImagesData } from '@/app/lib/data'; 
 import React, { useState } from 'react';
 import { Image_ } from '@/app/types/productTypes';
-
-//interface ImageCarouselProps{
-//    productId: number;
-//}
 
 interface ImageCarouselProps {
     images: Image_[];
@@ -18,37 +13,40 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
     //when setCurrentIndex is called, it re-renders the component w/ the updated state.
     const [currentIndex, setCurrentIndex] = useState(0);
     //useState<string[]>([]) sets an empty array as initial state
+    const [useDefaultImage, setUseDefaultImage] = useState(false);
+
         //may need to change this?
     //setImageUrls is function used to update state of imageUrls
     //const [imageUrls, setImageUrls] = useState<string[]>([]);
     
     const imageUrls = images.map(image => image.image_url);
-
-    
-    //useEffect = hook to run side effects in components.  takes 2 arguments, a function and an optional dependency array
-    //
-    //useEffect(() => {
-    //    // Fetch images when the component mounts
-    //    fetchImages();
-    //}, []);//empty dependnency means it runs only once at render(variables would mean it runs every time that variaable changed)
+    const defaultImageUrl = '/product-images/default_image.jpg';
 
     const handleNextImage = () => {
         // Increment index and loop back to 0 if it reaches the end
         setCurrentIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+        setUseDefaultImage(false);
     };
     const handlePrevImage = () => {
         //decrement index and loop back to 0 if it reaches end
         setCurrentIndex((prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length);
+        setUseDefaultImage(false);
+    };
+
+    const handleImageError = () => {
+        setUseDefaultImage(true); // Switch to default image if an error occurs
     };
 
     return (
         <div>
             {imageUrls.length > 0 ? (
                 <Image
-                    src={imageUrls[currentIndex]}
+                    //src={imageUrls[currentIndex]}
+                    src={useDefaultImage ? defaultImageUrl : imageUrls[currentIndex]}
                     alt={`Image ${currentIndex}`}
                     width={500}
                     height={500}
+                    onError={handleImageError}
                 />
             ) : (
                 <p>Loading...</p>
