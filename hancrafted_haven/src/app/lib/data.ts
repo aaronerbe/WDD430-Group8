135b2870d9c822a57abdff60c2f836d0069f88d3
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-10-20 00:04:33 -0600

    added update images (which is really a combo update/remove queries), edit product data

--- hancrafted_haven/src/app/lib/data.ts
+++ hancrafted_haven/src/app/lib/data.ts
@@ -5,15 +5,17 @@ import {Product, Image_, User, Review_} from '@/app/types/productTypes'
 export async function fetchProductData(productId: number): Promise<Product> {
     try {
         // Fetching the product by id
-        const result = await sql`SELECT id, user_id, name, description, price, category FROM products WHERE id = ${productId}`;
-        // have to break out the query result into structured format
+        const result = await sql`
+            SELECT id, user_id, name, description, price, category FROM products 
+            WHERE id = ${productId}
+        `;
+
         const product: Product = {
             id: result.rows[0].id,
             user_id: result.rows[0].user_id,
             name: result.rows[0].name,
             description: result.rows[0].description,
             price: result.rows[0].price,
-            //image_url: result.rows[0].image_url,
             category: result.rows[0].category,
         };
 
@@ -24,10 +26,63 @@ export async function fetchProductData(productId: number): Promise<Product> {
     }
 }
 
+export async function editProductData(productId: number, userId: number, name: string, description: string, price: number, category: string){
+    try {
+        //Update Product Data
+        //only allow updating name, description, price, category
+        const result = await sql`
+            UPDATE products
+            SET
+                name = ${name},
+                description = ${description},
+                price = ${price},
+                category = ${category}
+            WHERE id = ${productId} AND user_id = ${userId}
+        `;
+        console.log('Update product:', result); 
+    } catch (error) {
+        console.error('Database Error: ', error);
+        throw new Error('Failed to update product table '); 
+    }
+}
+
+export async function addProduct(userId: number, name: string, description: string, price: number, category: string): Promise<number> {
+    console.log('Inserting product:', {userId, name, description, price, category });
+    //given a userId + product information, create a new entry in product table.  
+    try {
+        // Perform the insert operation
+        const result = await sql`
+            INSERT INTO products ( 
+                user_id, 
+                name, 
+                description, 
+                price, 
+                category
+            )
+            VALUES (
+                ${userId},
+                ${name},
+                ${description}, 
+                ${price},
+                ${category}
+            )
+            RETURNING id;
+        `;
+        return result.rows[0].id; //gives back the id of the new product
+    } catch (error) {
+        console.error('Database Error: ', error);
+        throw new Error('Failed to create product '); 
+    }
+}
+
 export async function fetchImagesData(productId: number): Promise<Image_[]> {
     try {
-        // Fetching the product by id
-        const result = await sql`SELECT id, product_id, image_url FROM product_images WHERE product_id = ${productId}`;
+        const result = await sql`
+            SELECT id, 
+            product_id, 
+            image_url 
+            FROM productImages 
+            WHERE product_id = ${productId}`;
         // Map the query result into an array of Image_ objects
         const images: Image_[] = result.rows.map(row => ({
             id: row.id,
@@ -37,7 +92,68 @@ export async function fetchImagesData(productId: number): Promise<Image_[]> {
         return images; 
     } catch (error) {
         console.error('Database Error: ', error);
-        throw new Error('Failed to fetch image data. at data.ts');
+        throw new Error('Failed to fetch image data');
+    }
+}
+
+export async function editImagesData(productId: number, addImageUrl?: string[], deleteImageUrl?:string[], ){
+    //pass in what images you want to add or delete as arrays.  
+    try {
+        //Delete Image Data
+        if(deleteImageUrl && deleteImageUrl.length > 0){
+            for (const imageUrl of deleteImageUrl){
+                await sql`
+                    DELETE from productImages
+                    WHERE product_id = ${productId} AND image_url = ${imageUrl}
+                `;
+                console.log('removed ', imageUrl)
+            }
+        }
+        //Add Image Data
+        if(addImageUrl && addImageUrl.length > 0){
+            for (const imageUrl of addImageUrl){
+                await sql`
+                    INSERT INTO productImages (
+                        product_id, 
+                        image_url
+                    )
+                    VALUES (
+                        ${productId}, 
+                        ${imageUrl}
+                    )
+                    RETURNING *
+                `;
+                console.log('Added ', imageUrl)
+            }
+        }
+    } catch (error) {
+        console.error('Database Error: ', error);
+        throw new Error(`Failed to update image data`); 
+    }
+}
+
+export async function addImages(productId: number, addImageUrl: string[]){
+    try {
+        //Add Image Data
+        if(addImageUrl && addImageUrl.length > 0){
+            for (const imageUrl of addImageUrl){
+                const result = await sql`
+                    INSERT INTO productImages (
+                        product_id, 
+                        image_url
+                    )
+                    VALUES (
+                        ${productId}, 
+                        ${imageUrl}
+                    )
+                    RETURNING *
+                `;
+                console.log('Added ', result)
+            }
+        }
+    } catch (error) {
+        console.error('Database Error: ', error);
+        throw new Error(`Failed to add new image ${addImageUrl}`); 
     }
 }
 
@@ -45,7 +161,11 @@ export async function fetchUserData(userId: number): Promise<User>{
 
     try {
         // Fetching the product by id
-        const result = await sql`SELECT id, name, bio, email, password FROM users WHERE id = ${userId}`;
+        const result = await sql`
+            SELECT id, name, profile, bio, email, password, type
+            FROM users 
+            WHERE id = ${userId}
+        `;
 
         // have to break out the query result into structured format
         const user: User = {
