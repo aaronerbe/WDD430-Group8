497d09540b6850982fee60e443a8334acb67208c
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-10-23 17:45:13 -0600

    another attemp.  added suspense around the navbar where it calls the search component that uses useSearchParms()

--- hancrafted_haven/src/app/Navbar.js
+++ hancrafted_haven/src/app/Navbar.js
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { UserCircleIcon } from '@heroicons/react/24/outline';
 import Search from '@/app/ui/search';
+import {Suspense} from 'react';
 
 export default function Header() {
 
@@ -12,7 +13,10 @@ export default function Header() {
 				<div className="px-2">
                     <Link href="/">Home</Link>
                 </div>
-                <Search placeholder="Find a product..." />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Search placeholder="Find a product..." />
+                </Suspense>
+
                 <div className="px-2">
                     <UserCircleIcon className="h-[24px] w-[24px] -translate-x-1 -translate-y-1" />
                 </div>
