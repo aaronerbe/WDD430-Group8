897d2d80f1592656ce023b166ec75fd892c204e0
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-10-23 17:28:32 -0600

    making custom 404 to see if it makes nextjs happy when rendering?

---
+++ hancrafted_haven/src/app/404.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+export default function NotFound() {
+
+    const defaultImageUrl = '/product-images/default_image.jpg';
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen text-center">
+            <h1 className="text-2xl font-bold mb-4">Page Not Found</h1>
+            <p className="mb-4">Sorry, the page you are looking for does not exist.</p>
+            <div className="flex justify-center mb-4">
+                <Image
+                    src={defaultImageUrl}
+                    alt={`A sad, lost bunny`}
+                    width={500}
+                    height={500}
+                    priority
+                />
+            </div>
+            <button 
+                className={`mt-8 px-4 py-2 text-white bg-blue-700 transition-transform rounded hover:bg-blue-500`}>
+                <a href="/">Return to Home</a>
+
+            </button>
+
+        </div>
+    );
+};
