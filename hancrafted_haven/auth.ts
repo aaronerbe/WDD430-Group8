11445be6f3f1017a4a924ca2ced3a8f9016669c5
lagoke <72529997+lagoke@users.noreply.github.com>
2024-10-25 18:31:22 -0400

    action.ts and auth.ts modifications

--- hancrafted_haven/auth.ts
+++ hancrafted_haven/auth.ts
@@ -3,46 +3,47 @@ import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
-import type { User } from '@/app/lib/definitions';
+import type { User as AppUser } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
+import type { User as AuthUser } from 'next-auth';
 
-
+// Ensure the User type from your project matches the User type from @auth/core
+interface User extends AuthUser {
+  id: string; // Change id to string to match @auth/core User type
+  email: string;
+  password: string;
+}
 
 async function getUser(email: string): Promise<User | undefined> {
-    try {
-      const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-      return user.rows[0];
-    } catch (error) {
-      console.error('Failed to fetch user:', error);
-      throw new Error('Failed to fetch user.');
-    }
+  try {
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
   }
-   
+}
 
- 
-  export const { auth, signIn, signOut } = NextAuth({
-    ...authConfig,
-    providers: [
-      Credentials({
-        async authorize(credentials) {
-          const parsedCredentials = z
-            .object({ email: z.string().email(), password: z.string().min(6) })
-            .safeParse(credentials);
-   
-          if (parsedCredentials.success) {
-            const { email, password } = parsedCredentials.data;
-            const user = await getUser(email);
-            if (!user) return null;
-            const passwordsMatch = await bcrypt.compare(password, user.password);
-            if (passwordsMatch) return user;
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
 
-          }
-   
-          console.log('Invalid credentials');
-          return null;
-          
-        },
-      }),
-    ],
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
 
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
 });
