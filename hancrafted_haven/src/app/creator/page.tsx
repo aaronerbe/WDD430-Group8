7476f09474eddf2e79b323a1395ca6d659906f24
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-10-27 00:28:51 -0600

    commenting out unised page.tsx that's causing conflicts

--- hancrafted_haven/src/app/creator/page.tsx
+++ hancrafted_haven/src/app/creator/page.tsx
@@ -1,56 +1,56 @@
-import { fetchUserCreatorData} from '@/app/lib/data';
-import CreatorDetail from "@/app/components/CreatorDetail"
-import { User } from '@/app/types/productTypes'; 
-import {notFound} from 'next/navigation'
-//import {redirect} from 'next/navigation'
-/*
-interface Params {
-    params: {
-        productId: number; 
-        creatorId: number;
-    };
-}
+//import { fetchUserCreatorData} from '@/app/lib/data';
+//import CreatorDetail from "@/app/components/CreatorDetail"
+//import { User } from '@/app/types/productTypes'; 
+//import {notFound} from 'next/navigation'
+////import {redirect} from 'next/navigation'
+///*
+//interface Params {
+//    params: {
+//        productId: number; 
+//        creatorId: number;
+//    };
+//}
 
 
-*/
-export default async function CreatorDetailsPage() {
-   // const { productId } = params;
+//*/
+//export default async function CreatorDetailsPage() {
+//   // const { productId } = params;
 
-    try{
-        //FETCH DATA (server side) and pass it to the component (client side)
-      //  const productData: Product = await fetchProductData(productId);
-        //if (!productData) {return notFound()};
-       // const imageData: Image_[] = await fetchImagesData(productId);
-       // const reviewData: Review_[] = await fetchReviewData(productId)
+//    try{
+//        //FETCH DATA (server side) and pass it to the component (client side)
+//      //  const productData: Product = await fetchProductData(productId);
+//        //if (!productData) {return notFound()};
+//       // const imageData: Image_[] = await fetchImagesData(productId);
+//       // const reviewData: Review_[] = await fetchReviewData(productId)
         
-        // If productData doesn't exist, redirect to the custom 404 page
-      //  if (!productData) {
-         //   notFound();
-      //  }      
-        const creatorData: User = await fetchUserCreatorData()
-        if (!creatorData) {
-            notFound();
-        }  
-       // const otherProductData: Product[] = await fetchProductsByUser(creatorData.id)
+//        // If productData doesn't exist, redirect to the custom 404 page
+//      //  if (!productData) {
+//         //   notFound();
+//      //  }      
+//        const creatorData: User = await fetchUserCreatorData()
+//        if (!creatorData) {
+//            notFound();
+//        }  
+//       // const otherProductData: Product[] = await fetchProductsByUser(creatorData.id)
         
-        //render product details
-        return (
-            <CreatorDetail
-           // product={productData} 
-           // images={imageData} 
-            user={creatorData} 
-           // reviews={reviewData} 
-           //products={otherProductData}
-            />
-        );
-    }catch (error) {
-        console.error("Failed to fetch creator data:", error);
-        //return <div>Product not found.</div>;
-        return notFound();
-    }
+//        //render product details
+//        return (
+//            <CreatorDetail
+//           // product={productData} 
+//           // images={imageData} 
+//            user={creatorData} 
+//           // reviews={reviewData} 
+//           //products={otherProductData}
+//            />
+//        );
+//    }catch (error) {
+//        console.error("Failed to fetch creator data:", error);
+//        //return <div>Product not found.</div>;
+//        return notFound();
+//    }
 
 
 
 
-}
+//}
 
