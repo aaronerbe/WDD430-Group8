7a3a131ab51629ab8288925c02b73936ae7db457
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-10-27 00:31:03 -0600

    has to have some content I guess.

--- hancrafted_haven/src/app/creator/page.tsx
+++ hancrafted_haven/src/app/creator/page.tsx
@@ -54,3 +54,31 @@
 
 //}
 
+import Image from 'next/image';
+
+const ProductNotFound = () => {
+    const defaultImageUrl = '/product-images/default_image.jpg';
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen text-center">
+            <h1 className="text-2xl font-bold mb-4">Not a valid page</h1>
+            <p className="mb-4">Sorry, you should not have been able to get here.</p>
+            <div className="flex justify-center mb-4">
+                <Image
+                    src={defaultImageUrl}
+                    alt={`A sad, lost bunny`}
+                    width={500}
+                    height={500}
+                    priority
+                />
+            </div>
+            <button 
+                className={`mt-8 px-4 py-2 text-white bg-blue-700 transition-transform rounded hover:bg-blue-500`}>
+                <a href="/">Return to Home</a>
+
+            </button>
+
+        </div>
+    );
+};
+
+export default ProductNotFound;
