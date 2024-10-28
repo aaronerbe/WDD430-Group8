724297ead7fd560b31c9d9a0f98f1ef1e04de118
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2024-10-28 17:37:43 -0600

    tweaked add review so it can auto refresh the content.  Also fixed seed data so it will auto-increment id's correctly.

--- hancrafted_haven/src/app/api/addReview/route.ts
+++ hancrafted_haven/src/app/api/addReview/route.ts
@@ -1,11 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { addReview } from '@/app/lib/data';
+import { addReview, fetchReviewData} from '@/app/lib/data';
 
 export async function POST(req: NextRequest) {
     const { productId, userId, rating, comment } = await req.json();
     try {
         await addReview(productId, userId, rating, comment);
-        return NextResponse.json({ message: 'Review added successfully!' });
+        //return NextResponse.json({ message: 'Review added successfully!' });
+        
+        const newReviews = await fetchReviewData(productId)
+        return NextResponse.json({ 
+            message: 'Review added successfully!', 
+            reviews: newReviews 
+        });
     } catch (error) {
         console.error('Failed to add review:', error);
         return NextResponse.json({ error: 'Failed to add review.' }, { status: 500 });
